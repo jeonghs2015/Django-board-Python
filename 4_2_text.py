3103f450249dc5710a7083f4257ae7c637bf533c
hsjung2015 <hsjung201558@gmail.com>
2022-05-26 14:44:31 +0900

    문자열 출력, 결합, 변경

---
+++ 4_2_text.py
@@ -0,0 +1,16 @@
+print("Hello world!!!")
+print('Hello world!!!')
+print('''hello1
+hello2
+hello3
+''')
+print("""world1
+world2
+world3
+""")
+
+print("'1'+'1'", '1'+'1')
+print("'a'+'b'", 'a'+'b')
+print("'Hello'+'World'", 'Hello '+' World')
+print("len('Hello World')", len('Hello World'))
+print("'Jeonghs'.replace('Jeong','J')", 'Jeonghs'.replace('Jeong','J'))
