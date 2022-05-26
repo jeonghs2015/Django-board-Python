cecf4e0f720592d6158c24912bf0ed14ea3edf02
hsjung2015 <hsjung201558@gmail.com>
2022-05-26 14:36:55 +0900

    모듈 import(math, random)

---
+++ 4_1_number.py
@@ -0,0 +1,19 @@
+import math
+
+
+print(-1)
+print(0)
+print(1)    #int
+print(1.1)  #float
+print('1+1', 1+1)
+print('1-1', 2-1)
+print('3*3', 3*3)
+print('4/2', int(4/2))
+
+import math
+print('math.sqrt(4)', math.sqrt(4))
+print('math.pow(4,2)', math.pow(4,2))
+
+
+import random
+print('random.random()',random.random())
