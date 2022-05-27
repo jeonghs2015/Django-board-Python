cc92e0c4cbdc9310e60eefdf5bff1bd44dc6dd50
hsjung2015 <hsjung201558@gmail.com>
2022-05-27 10:52:00 +0900

    basic for문

---
+++ tutorials/control_flow/5_1_for_basic.py
@@ -0,0 +1,8 @@
+names = ['Hackett', 'Dauda', 'Wolf']
+for name in names:
+    print('Hello ' + name + ' , welcome. ' + '''
+enjoy your time. 
+have a nice day ''' + name)
+    print('''
+--------------
+''')
