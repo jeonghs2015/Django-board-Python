638f5bad1ade23caab6c4c2251c3b53078df3c50
hsjung2015 <hsjung201558@gmail.com>
2022-05-27 16:26:11 +0900

    index page, read page 구현

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,49 @@
 from django.http import HttpResponse
+import random
+
 from django.shortcuts import render
 
 # Create your views here.
+topics = [
+    {'id':1, 'title':'routing', 'body' : 'Routing is ...'},
+    {'id':2, 'title':'view', 'body' : 'View is ...'},
+    {'id':3, 'title':'model', 'body' : 'Model is ...'},
+]
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]} </a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}<br>
+        <ul>
+            <li><a href="/create/">Create</a></li>
+        </ul>
+    </body>
+    </html>
+    '''
+
 def index(request):
-    return HttpResponse('Welcome!')
+    article = '''
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def create(request):
     return HttpResponse('Create!')
 
 def read(request, id):
-    return HttpResponse('Read - ' + id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
