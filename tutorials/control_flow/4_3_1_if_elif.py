c9b9d28315d4eb2fc5bec87de8fb804e096493f9
hsjung2015 <hsjung201558@gmail.com>
2022-05-27 10:02:08 +0900

    elif 형식

---
+++ tutorials/control_flow/4_3_1_if_elif.py
@@ -0,0 +1,8 @@
+# 014
+print(0)
+if True:
+    print('if-true')
+elif True:
+    print('elif-true')
+else :
+    print('else')
---
+++ tutorials/control_flow/4_3_2_elif_app.py
@@ -0,0 +1,9 @@
+input_id = input('id : ')
+id1 = 'jeonghs'
+id2 = 'dauda'
+if(input_id == id1):
+    print('Welcome jeonghs!!!')
+elif(input_id == id2):
+    print('Welcome dauda!!!')
+else:
+    print('who are you?')
